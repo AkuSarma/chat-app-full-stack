b017cb07260000766c14a531e84cfc8192a62240
Aku Sarma <akusarma1702@gmail.com>
2024-10-30 06:38:45 +0530

    fix error

--- react.js_app/src/components/Search.js
+++ react.js_app/src/components/Search.js
@@ -1,99 +1,123 @@
 import { useContext, useState } from "react";
 import UserContext from "../UserContext";
-import { Autocomplete } from '@mui/material'
-import { TextField } from "@mui/material";
-import "./Search.css"
+import { Autocomplete, TextField } from "@mui/material";
+import "./Search.css";
 
-const Search = ({filteredFun, addFriend}) => {
-    const {loggedInUser, users} = useContext(UserContext);
-    const [searchedUser, setSearchedUser] = useState([])
-    const [isFriend, setIsFriend] = useState(false)
-    const [inputValue, setInputValue] = useState("")
+const Search = ({ filteredFun, addFriend }) => {
+  const { loggedInUser, users } = useContext(UserContext);
+  const [searchedUser, setSearchedUser] = useState([]);
+  const [isFriend, setIsFriend] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
-    const optionsObjectArr = users.filter(user => user.username.toLowerCase() !== loggedInUser.username.toLowerCase())
+  const optionsObjectArr = users.filter(
+    (user) =>
+      user.username &&
+      loggedInUser &&
+      user.username.toLowerCase() !== loggedInUser.username.toLowerCase()
+  );
 
-    const options = optionsObjectArr.map(user => user.username)
+  const options = optionsObjectArr.map((user) => user.username);
 
-    const filteredUser = () => users.find(user => {
-            if(user.username.toLowerCase() === (inputValue.toLowerCase())){
-                setSearchedUser(user)
-                return user
-            }else{
-                return null
-            }
-        })
+  const filteredUser = () => {
+    const foundUser = users.find((user) => {
+      if (
+        user.username &&
+        inputValue &&
+        user.username.toLowerCase() === inputValue.toLowerCase()
+      ) {
+        setSearchedUser(user);
+        return user;
+      } else {
+        return null;
+      }
+    });
+    return foundUser;
+  };
 
-    const filteredFriends = username => {
-        return loggedInUser.friends.find(friend => {
-            if(friend.username.toLowerCase() === username.toLowerCase()){
-                setSearchedUser(friend)
-                return friend
-            }else{
-                return null
-            }
-        })
-    }
+  const filteredFriends = (username) => {
+    return loggedInUser.friends.find((friend) => {
+      if (
+        friend.username &&
+        username &&
+        friend.username.toLowerCase() === username.toLowerCase()
+      ) {
+        setSearchedUser(friend);
+        return friend;
+      } else {
+        return null;
+      }
+    });
+  };
 
-    const isFindFriend = () => {
-        return loggedInUser.friends.find(friend => {
-            const isFind = users.find(user => {
-                if(user._id === friend._id){
-                    return user.username.toLowerCase() === inputValue.toLowerCase()
-                }else{
-                    return false
-                }
-            })
-            setIsFriend(isFind)
-            return isFind
-        })
-    }
-
-    const handleSubmit = event => {
-        event.preventDefault()
-        if(inputValue === "") filteredFun(loggedInUser.friends)
-        const friend = filteredFriends(inputValue)
-        if(isFindFriend() && friend){
-            filteredFun([friend])
+  const isFindFriend = () => {
+    return loggedInUser.friends.find((friend) => {
+      const isFind = users.find((user) => {
+        if (user._id === friend._id && user.username) {
+          return user.username.toLowerCase() === inputValue.toLowerCase();
+        } else {
+          return false;
         }
-        setInputValue("")
-    }
+      });
+      setIsFriend(isFind);
+      return isFind;
+    });
+  };
 
-    const handleClickToAddFriend = () => {
-        if(isFindFriend() === undefined){
-            const user = filteredUser()
-            addFriend(user)
-            setInputValue("")
-        }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue === "") filteredFun(loggedInUser.friends);
+    const friend = filteredFriends(inputValue);
+    if (isFindFriend() && friend) {
+      filteredFun([friend]);
     }
+    setInputValue("");
+  };
 
-return(
+  const handleClickToAddFriend = () => {
+    if (!isFindFriend()) {
+      const user = filteredUser();
+      if (user) {
+        addFriend(user);
+      }
+      setInputValue("");
+    }
+  };
+
+  return (
     <div className="search">
-        <Autocomplete
-            freeSolo
-            disableClearable
-            options={options}
-            inputValue={inputValue}
-            onInputChange={(event, newInputValue) => {
-            setInputValue(newInputValue);
+      <Autocomplete
+        freeSolo
+        disableClearable
+        options={options}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search username"
+            InputProps={{
+              ...params.InputProps,
+              type: "search",
             }}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="Search username"
-                    InputProps={{
-                    ...params.InputProps,
-                    type: 'search',
-                    }}
-                />
-              )}
-        />
-        <div className="right">
-            <button id="search-btn" onClick={handleSubmit} alt="search button">🔍</button>
-            <button id="add-btn" onClick={handleClickToAddFriend} alt="add friend button">➕</button>
-        </div>
+          />
+        )}
+      />
+      <div className="right">
+        <button id="search-btn" onClick={handleSubmit} alt="search button">
+          🔍
+        </button>
+        <button
+          id="add-btn"
+          onClick={handleClickToAddFriend}
+          alt="add friend button"
+        >
+          ➕
+        </button>
+      </div>
     </div>
-)
-
-}
+  );
+};
 
 export default Search;
